9b54d7dbdd792006c8cf57ef9ae494c6d0749e74
DominikSchmidt <dominikschmidt1893@gmail.com>
2016-09-02 11:24:30 -0700

    added asciiart1 and first prog
    added asciiart1 and first prog

---
+++ APLesson_01/ASCART1.java
@@ -0,0 +1,15 @@
+public class HI
+{
+	public static void main(String[]args)
+	{
+		System.out.println("            ");
+		System.out.println("    __  ___ ");
+		System.out.println("   / /  /  (_)");
+		System.out.println("  / /_/  / / ");
+		System.out.println(" / __   / /  ");
+		System.out.println("/_/ /_/_/   ");
+		System.out.println("            ");
+	}
+		
+	
+}
