bad677be0794ead8c8bb1e700820ce8e255a69b7
DominikSchmidtqq <dominikschmidt1893@gmail.com>
2016-10-26 21:24:50 -0700

    changed folder
    /*

--- AP_Lesson_05/5.2/Ex06.java
+++ AP_Lesson_05/5.2/Ex06.java
@@ -1,9 +1,43 @@
 import java.util.Scanner;
-public class Ex05
+public class Ex06
 {
+	static Scanner kb;
 	public static void main(String[]args)
 	{
-		Scanner kb = new Scanner(System.in);
+		kb = new Scanner(System.in);
+		String username = "User";
+		String password = "Password";
 		
+		passCheck();
 	}
+	public static void passCheck()
+	{
+		System.out.println("What is the Username?");
+		String entUsername = kb.next();
+		System.out.println("What is the Password");
+		String entPassword = kb.next();
+		if (entUsername.equals("User"))
+		{
+			if (entPassword.equals("Password"))
+				System.out.println("Your Password and Username are right. You are granted access.");
+		
+			else
+				System.out.println("Your username is wrong but your password is right. Try again.");
+				passCheck();
+		}
+		else
+		{
+			if (entPassword.equals("Password"))
+			{
+				System.out.println("Your Username is wrong but the password is right try again.");
+				passCheck();
+			}
+			else
+			{
+				System.out.println("Your password and username are wrong. Try again.");
+				passCheck();
+			}
+		}
+	}
+	
 }
