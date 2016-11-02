021de408875d29c5b8cfa8498847967f30191618
DominikSchmidtqq <dominikschmidt1893@gmail.com>
2016-11-02 11:51:55 -0700

    fixed receipt
    1232

--- AP_Lesson_04/Ex_01.java
+++ AP_Lesson_04/Ex_01.java
@@ -23,33 +23,34 @@ public class Ex_01
 		System.out.println("Enter price");
 		double price3 = kb.nextDouble();
 		
-		form.format(item1, price1);
-		form.format(item2, price2);
-		form.format(item3, price3);
+		
 		
 		
 		double total = price1 + price2 + price3;
-		form.format("Subtotal", total);
+		
 		
 		double price4 = total * .1;
 		String item4 = "tax";
 		
-		form.format(item4, price4);
+		
 		
 		double price5 = price4 + total;
 		String item5 = ("Total");
+		System.out.println("<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>");
+		form.format(item1, price1);
+		form.format(item2, price2);
+		form.format(item3, price3);
+		form.format(item4, price4);
+		form.format("Subtotal", total);
 		form.format(item5, price5);
-		
-		
-		
-		
-		
+		System.out.println("\n<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>");
+
 	}
 	
 	public void format(String item,  double price)
 	{
 		
-		System.out.printf("\n%20s  %20.2f", item, price);
+		System.out.printf("\n%17s  %17.2f", item, price);
 		
 		
 	}
