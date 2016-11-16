5b6cdbd66ef3e202687f99738900fccc61cbddc8
DominikSchmidtqq <dominikschmidt1893@gmail.com>
2016-11-15 18:29:13 -0800

    finished 9
    /*

---
+++ APLesson_09/Ex02.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Ex02{
+	public static void main(String args[]){
+		Scanner kb = new Scanner(System.in);
+		System.out.println("Please enter 5 words.");
+		String [] words = new String[5];
+		for(int i=0; i<words.length; i++){
+			words[i]=kb.next();
+		}
+		first(words);
+	}
+	public static void first(String[]words){
+		for(String word : words){
+			System.out.println();
+			System.out.print(word.substring(0,1));
+		}
+	}
+}
---
+++ APLesson_09/Ex03.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Ex03{
+	public static void main(String args[]){
+		int [] numbers = new int[10];
+		for(int i=0; i<numbers.length; i++){
+			numbers[i]=(int)(Math.random()*100)+1;
+		}
+		System.out.println("Numbers");
+		for( num:numbers){
+			System.out.println(num+ " ");
+		}
+		System.out.println();
+		System.out.println("The average of the above numbers is "+ average(numbers));
+	}
+	public static void average(int[] numbers){
+		double average=0;
+		for(int num:numbers){
+			average +=num;
+		}
+		average/=numbers.length;
+		return average;
+	}
+}
