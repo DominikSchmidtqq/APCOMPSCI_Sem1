8c23374a622c05fdf0cdc377e6439e29808402b8
Dominik Schmidt <dominikschmidt1893@gmail.com>
2016-12-01 22:43:26 -0800

    Update Ex_04.java

--- APLesson_07/Ex_04.java
+++ APLesson_07/Ex_04.java
@@ -1,14 +1,12 @@
 import java.util.Scanner;
-public class Ex_04
-{
-	public static void main(String[]args)
-	{
+public class Ex_04 {
+	public static void main(String[]args) {
 		Scanner kb = new Scanner(System.in);
 		System.out.println("Please enter a String");
 		String line = kb.next();
-		while(line.indexOf("a") >= 0)
-		{
-		line = line.substring(0, line.indexOf("a")) + "@"+ line.substring(line.indexOf("a")+1);
+		while(line.indexOf("a") >= 0) {
+		line = line.substring(0, line.indexOf("a")) + "@" +line.substring(line.indexOf("a")+1, line.length());
+        }
 		}
 		System.out.println(line);
 	}
