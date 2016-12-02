5836d7a33441682cd5151fc7613d4a3158cbd957
DominikSchmidtqq <dominikschmidt1893@gmail.com>
2016-12-01 23:05:25 -0800

    fixed 4
    /*

--- APLesson_07/Ex_04.java
+++ APLesson_07/Ex_04.java
@@ -3,11 +3,10 @@ public class Ex_04 {
 	public static void main(String[]args) {
 		Scanner kb = new Scanner(System.in);
 		System.out.println("Please enter a String");
-		String line = kb.next();
+		String line = kb.nextLine();
 		while(line.indexOf("a") >= 0) {
 		line = line.substring(0, line.indexOf("a")) + "@" +line.substring(line.indexOf("a")+1, line.length());
         }
-		}
 		System.out.println(line);
 	}
 }
