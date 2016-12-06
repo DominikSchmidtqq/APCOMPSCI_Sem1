dea112539a2a767a3d33cb8db36173f86b86fcc9
DominikSchmidtqq <dominikschmidt1893@gmail.com>
2016-12-06 11:32:33 -0800

    added 2 and 3
    /*

---
+++ APLesson_11/Ex_03.java
@@ -0,0 +1,22 @@
+public class Ex_03 {
+	public static void main(String args[]) {
+		String [][] xAndO = new String [4][4];
+		for(int i=0; i<xAndO.length; i++) {
+			for (int j=0; j<xAndO[i].length; j++) {
+				int xoro = (int)(Math.random()*2)+1;
+				if(xoro == 1) {
+					xAndO[i][j]="x";
+				}
+				else if(xoro == 2) {
+					xAndO[i][j]="o";
+				}
+			}
+		}
+		for(int i=0; i<xAndO.length; i++) {
+			for(int d = 0; d<xAndO[i].length; d++) {
+				System.out.println(xAndO[i][d]);
+			}
+			System.out.println();
+		}
+	}
+}
