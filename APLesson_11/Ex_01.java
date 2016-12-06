ef989800fdc30b9885e7127dd329aac22e281a72
DominikSchmidtqq <dominikschmidt1893@gmail.com>
2016-12-06 10:58:15 -0800

    added 11
    /*

---
+++ APLesson_11/Ex_01.java
@@ -0,0 +1,17 @@
+public class Ex_01 {
+	public static void main(String args[]) {
+		int[][] nums = new int [4][4];
+		for(int i=0; i < nums.length; i++) {
+			for (int j=0; j<nums[i].length; j++) {
+				nums[i][j] = (int)(Math.random()*50)+1;
+
+			}
+		}
+		for(int i=0; i<nums.length; i++) {
+			for(int d = 0; d<nums[i].length; d++) {
+				System.out.println(nums[i][d]);
+			}
+			System.out.println();
+		}
+	}
+}
