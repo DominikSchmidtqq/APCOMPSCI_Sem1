66b5542b44441c7b7c8043938ddd2d7a565debb9
DominikSchmidtqq <dominikschmidt1893@gmail.com>
2017-01-24 10:22:54 -0800

    fixed Ex03

--- APLesson_09/Ex03.java
+++ APLesson_09/Ex03.java
@@ -1,23 +1,21 @@
-import java.util.Scanner;
-public class Ex03{
-	public static void main(String args[]){
-		int [] numbers = new int[10];
-		for(int i=0; i<numbers.length; i++){
-			numbers[i]=(int)(Math.random()*100)+1;
-		}
-		System.out.println("Numbers");
-		for(int num:numbers){
-			System.out.println(num+ " ");
-		}
-		System.out.println();
-		System.out.println("The average of the above numbers is "+ average(numbers));
-	}
-	public static void average(int[] numbers){
-		double average=0;
-		for(int num:numbers){
-			average +=num;
-		}
-		average/=numbers.length;
-		return average;
-	}
+public class Ex03 {
+    public static void main(String args[]) {
+        int[] numbers = new int[10];
+        for (int i=0; i<numbers.length; i++) {
+            numbers[i] = (int)(Math.random() * 100) + 1;
+        }
+        System.out.println("Numbers:");
+        for (int num : numbers) {
+            System.out.print(num + " ");
+        }
+        System.out.println("\nThe average of the above numbers is: " + average(numbers));
+    }
+    public static double average(int[] n) {
+        double average = 0;
+        for (int num : n) {
+            average += num;
+        }
+        average /= n.length;
+        return average;
+    }
 }
