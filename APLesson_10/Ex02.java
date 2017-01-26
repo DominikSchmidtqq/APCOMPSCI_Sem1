c072a02b46c53ff819eada3ccfa0d0fa9ecf5572
Dominik Schmidt <dominikschmidt1893@gmail.com>
2017-01-26 08:35:11 -0800

    /*

--- APLesson_10/Ex02.java
+++ APLesson_10/Ex02.java
@@ -2,28 +2,14 @@ import java.util.*;
 public class Ex02 {
 	public static void main(String args[]){
 		Scanner kb = new Scanner(System.in);
-		System.out.println("Enter Equation to be solved ");
+		System.out.println("Enter Equation: ");
 		String eqString = kb.nextLine();
 		ArrayList<String> equation = new ArrayList<String>(Arrays.asList(eqString.split(" ")));
-		System.out.println("The answer is " +xEquation(equation)+" ,Duh!");
+		System.out.println(xEquation(equation));
 	}
 	public static ArrayList xEquation(ArrayList<String> equation){
-			int i = 0;
-			while(i<equation.size()){
-				if(equation.get(i).equals("+") || equation.get(i).equals("-")){
-					if(equation.get(i).equals("+")){
-						equation.set(i, Integer.toString((Integer.parseInt(equation.get(i-1))+Integer.parseInt(equation.get(i+1)))));
-					}
-					else {
-						equation.set(i, Integer.toString((Integer.parseInt(equation.get(i-1))-Integer.parseInt(equation.get(i+1)))));
-					}
-					equation.remove(i-1);
-					equation.remove(i);
-				}
-				i++;
-			}
-			while(equation.size()>1){
-			i=0;
+		while(equation.size()>1){
+			int i=0;
 			while(i<equation.size()){
 				if(equation.get(i).equals("*") || equation.get(i).equals("/")){
 					if(equation.get(i).equals("*")){
@@ -31,6 +17,20 @@ public class Ex02 {
 					}
 					else {
 						equation.set(i, Integer.toString((Integer.parseInt(equation.get(i-1))/Integer.parseInt(equation.get(i+1)))));
+					}
+					equation.remove(i-1);
+					equation.remove(i);
+				}
+				i++;
+			}
+			i=0;
+			while(i<equation.size()){
+				if(equation.get(i).equals("+") || equation.get(i).equals("-")){
+					if(equation.get(i).equals("+")){
+						equation.set(i, Integer.toString((Integer.parseInt(equation.get(i-1))+Integer.parseInt(equation.get(i+1)))));
+					}
+					else {
+						equation.set(i, Integer.toString((Integer.parseInt(equation.get(i-1))-Integer.parseInt(equation.get(i+1)))));
 					}
 					equation.remove(i-1);
 					equation.remove(i);
