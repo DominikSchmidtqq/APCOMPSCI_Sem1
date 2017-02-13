f8100d52b4a2880292ab2aafb6062dabc1f186c6
Dominik Schmidt <dominikschmidt1893@gmail.com>
2017-02-13 12:09:23 -0800

    update
    /*

---
+++ APLesson_13/13.2/TicketDriver.java
@@ -0,0 +1,8 @@
+public class TicketDriver {
+	static Scanner kb = new Scanner(System.in);
+	public static void main(String args[]) {
+		Ticket Walkup = new Walkup();
+		System.out.println(Walkup);
+		System.out.println("How many days are left?");
+	}
+}
