74873d04fbe855fe8368a8e64a2c536ace2d8f45
DominikSchmidtqq <dominikschmidt1893@gmail.com>
2017-03-02 08:14:27 -0800

    update
    /*

--- APLesson_14/Satellite.java
+++ APLesson_14/Satellite.java
@@ -1,6 +1,6 @@
 import java.util.ArrayList;
 public class Satellite {
-   public static void main(String[]args) {
+	public static void main(String[]args) {
 		ArrayList<Location> locate = new ArrayList<>();
 		double[] honLoc = {5, 6};
 		locate.add(new Honda(honLoc));
@@ -9,20 +9,19 @@ public class Satellite {
         double[] home = {0, 0};
         String printout = "\n\n" +"==========================" + "\nStarting locations...";
         for (Location l : locate) {
-           l.setID();
-		   printout += "\nLocation for " + l.getID() + ": (" + getLocation(l.getLoc()) + ")";
+			l.setID();
+			printout += "\nLocation for " + l.getID() + ": (" + getLocation(l.getLoc()) + ")";
         }
         printout += "\n\n" + "==========================" + "\nDistance from home...";
-        for (Location l : locate) {
-           printout += "\nDistance for " + l.getID() + ": (" + getDistance(l.getLoc(), home)+ ")";
-       }
-        System.out.println(printout);
+		for (Location l : locate) {
+			printout += "\nDistance for " + l.getID() + ": (" + getDistance(l.getLoc(), home)+ ")";
+		}
+		System.out.println(printout);
    }
-    public static double getDistance(double[] car, double[] home) {
-       return Math.sqrt((Math.pow(car[0] - home[0], 2)+ Math.pow(car[1] - home[1], 2)));
+   public static double getDistance(double[] car, double[] home) {
+	   return Math.sqrt((Math.pow(car[0] - home[0], 2)+ Math.pow(car[1] - home[1], 2)));
    }
-
-    public static String getLocation(double[] loc) {
-       return loc[0] + ", " + loc[1];
+   public static String getLocation(double[] loc) {
+	   return loc[0] + ", " + loc[1];
    }
 }
