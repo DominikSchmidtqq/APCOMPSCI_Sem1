104310f167cc2910fcc2fa42a970e485a072ffde
DominikSchmidtqq <dominikschmidt1893@gmail.com>
2017-05-08 12:00:14 -0700

    formatting
    /*

--- ElevensLab/CardTester.java
+++ ElevensLab/CardTester.java
@@ -1,12 +1,4 @@
-/**
- * This is a class that tests the Card class.
- */
 public class CardTester {
-
-	/**
-	 * The main method in this class checks the Card operations for consistency.
-	 *	@param args is not used.
-	 */
 	public static void main(String[] args) {
 		Card card1 = new Card("Heart", "9", 9);
 		Card card2 = new Card("Heart", "9", 9);
